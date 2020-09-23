2bad35bac54f625f20726ca0ed59a3524d62c28b
Anton <antongrem159@rambler.ru>
2020-09-23 23:36:30 +0300

    сделал с помощью случайного числа

--- js/setup.js
+++ js/setup.js
@@ -10,48 +10,29 @@ const similarListElement = document.querySelector(`.setup-similar-list`);
 const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
   .content
   .querySelector(`.setup-similar-item`);
-
-
-const shuffleArr = function shuffle(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-  return array;
+function Wizards(name, coatColor, eyesColor) {
+  this.name = name;
+  this.coatColor = coatColor;
+  this.eyesColor = eyesColor;
+}
+const randomNumber = function (min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-shuffleArr(WIZARD_NAMES);
-shuffleArr(WIZARD_SECOND_NAMES);
-shuffleArr(COAT_COLOR);
-shuffleArr(EYES_COLOR);
-const wizards = [
-  {
-    name: WIZARD_NAMES[0] + WIZARD_SECOND_NAMES[0],
-    coatColor: COAT_COLOR[0],
-    eyesColor: EYES_COLOR[0]
-  },
-  {
-    name: WIZARD_NAMES[1] + WIZARD_SECOND_NAMES[1],
-    coatColor: COAT_COLOR[1],
-    eyesColor: EYES_COLOR[1]
-  },
-  {
-    name: WIZARD_NAMES[2] + WIZARD_SECOND_NAMES[2],
-    coatColor: COAT_COLOR[2],
-    eyesColor: EYES_COLOR[2]
-  },
-  {
-    name: WIZARD_NAMES[3] + WIZARD_SECOND_NAMES[3],
-    coatColor: COAT_COLOR[3],
-    eyesColor: EYES_COLOR[3]
-  }
-];
-
 for (let i = 0; i < 4; i++) {
+  let randomN = randomNumber(0, WIZARD_NAMES.length - 1);
+  let randomN1 = randomNumber(0, WIZARD_SECOND_NAMES.length - 1);
+  let randomN2 = randomNumber(0, COAT_COLOR.length - 1);
+  let randomN3 = randomNumber(0, EYES_COLOR.length - 1);
+  let randomName = WIZARD_NAMES[randomN] + ` ` + WIZARD_SECOND_NAMES[randomN1];
+  let randomCoatColor = COAT_COLOR[randomN2];
+  let randomEyesColor = EYES_COLOR[randomN3];
+
+  let wizard = new Wizards(randomName, randomCoatColor, randomEyesColor);
   const wizardElement = similarWizardTemplate.cloneNode(true);
-  wizardElement.querySelector(`.setup-similar-label`).textContent = wizards[i].name;
-  wizardElement.querySelector(`.wizard-coat`).style.fill = wizards[i].coatColor;
-  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizards[i].eyesColor;
+  wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
+  wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
   similarListElement.appendChild(wizardElement);
 }

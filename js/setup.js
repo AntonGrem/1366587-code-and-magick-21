1db80dba0c477c2e3c70eac04ba80cfddfe79438
Anton <antongrem159@rambler.ru>
2020-09-30 11:03:59 +0300

    dfghjklhgfdfghjkl

--- js/setup.js
+++ js/setup.js
@@ -10,29 +10,57 @@ const similarListElement = document.querySelector(`.setup-similar-list`);
 const similarWizardTemplate = document.querySelector(`#similar-wizard-template`)
   .content
   .querySelector(`.setup-similar-item`);
-function Wizards(name, coatColor, eyesColor) {
+const userDialog = document.querySelector(`.setup`);
+userDialog.classList.remove(`hidden`);
+const Wizards = function (name, coatColor, eyesColor) {
   this.name = name;
   this.coatColor = coatColor;
   this.eyesColor = eyesColor;
-}
+};
 const randomNumber = function (min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-for (let i = 0; i < 4; i++) {
-  let randomN = randomNumber(0, WIZARD_NAMES.length - 1);
-  let randomN1 = randomNumber(0, WIZARD_SECOND_NAMES.length - 1);
-  let randomN2 = randomNumber(0, COAT_COLOR.length - 1);
-  let randomN3 = randomNumber(0, EYES_COLOR.length - 1);
-  let randomName = WIZARD_NAMES[randomN] + ` ` + WIZARD_SECOND_NAMES[randomN1];
-  let randomCoatColor = COAT_COLOR[randomN2];
-  let randomEyesColor = EYES_COLOR[randomN3];
+/* for (let i = 0; i < 4; i++) {
+  const randomN = randomNumber(0, WIZARD_NAMES.length - 1);
+  const randomN1 = randomNumber(0, WIZARD_SECOND_NAMES.length - 1);
+  const randomN2 = randomNumber(0, COAT_COLOR.length - 1);
+  const randomN3 = randomNumber(0, EYES_COLOR.length - 1);
+  const randomName = WIZARD_NAMES[randomN] + ` ` + WIZARD_SECOND_NAMES[randomN1];
+  const randomCoatColor = COAT_COLOR[randomN2];
+  const randomEyesColor = EYES_COLOR[randomN3];
 
-  let wizard = new Wizards(randomName, randomCoatColor, randomEyesColor);
+  const wizard = new Wizards(randomName, randomCoatColor, randomEyesColor);
   const wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector(`.setup-similar-label`).textContent = wizard.name;
   wizardElement.querySelector(`.wizard-coat`).style.fill = wizard.coatColor;
   wizardElement.querySelector(`.wizard-eyes`).style.fill = wizard.eyesColor;
   similarListElement.appendChild(wizardElement);
+}*/
+
+const renderWizard = function () {
+  const randomN = randomNumber(0, WIZARD_NAMES.length - 1);
+  const randomN1 = randomNumber(0, WIZARD_SECOND_NAMES.length - 1);
+  const randomN2 = randomNumber(0, COAT_COLOR.length - 1);
+  const randomN3 = randomNumber(0, EYES_COLOR.length - 1);
+  const randomName = WIZARD_NAMES[randomN] + ` ` + WIZARD_SECOND_NAMES[randomN1];
+  const randomCoatColor = COAT_COLOR[randomN2];
+  const randomEyesColor = EYES_COLOR[randomN3];
+  const wizardUser = new Wizards(randomName, randomCoatColor, randomEyesColor);
+  const wizardElement = similarWizardTemplate.cloneNode(true);
+  wizardElement.querySelector(`.setup-similar-label`).textContent = wizardUser.name;
+  wizardElement.querySelector(`.wizard-coat`).style.fill = wizardUser.coatColor;
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = wizardUser.eyesColor;
+  similarListElement.appendChild(wizardElement);
+
+  return wizardElement;
+};
+
+const fragment = document.createDocumentFragment();
+for (let i = 0; i < 4; i++) {
+  fragment.appendChild(renderWizard());
 }
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector(`.setup-similar`).classList.remove(`hidden`);
